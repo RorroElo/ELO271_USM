8570ab6358488acd840319b4a31794bec78270df
Rodrigo Avila <rodrigo.avilaca@usm.cl>
2024-04-15 01:58:04 -0400

    Ordene los archivos

--- grupo1_sesion1_exp1.m
+++
@@ -1,6 +0,0 @@
-u_1 = out.u_1;
-u_2 = out.u_2;
-w = out.w;
-y = out.y;
-Phi = [u_1 u_2];
-a = Phi\y;
--- grupo1_sesion1_exp1_act2.m
+++
@@ -1,7 +0,0 @@
-u_1 = out.u_1;
-u_2 = out.u_2;
-w = out.w;
-y = out.y;
-unos = ones(length(u_1),1);
-Phi = [unos u_1 u_2];
-a = Phi\y;
